8afb8eee8428322fe9865c26473fc693f45f79ed
Matt Gray <matt.gray@retrofuzz.com>
2016-06-30 22:10:42 +0100

    Handle gulp errors

--- containers/build/gulpfile.js
+++ containers/build/gulpfile.js
@@ -2,9 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
+
+var onError = function (err) {
+  console.log(err.toString());
+	this.emit('end');
+};
 
 gulp.task('scss', function() {
 	return gulp.src('src/scss/styles.scss')
+	  .pipe(plumber({
+		  errorHandler: onError
+	  }))
 		.pipe(sass({ errLogToConsole: true }))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest('src/css'))
