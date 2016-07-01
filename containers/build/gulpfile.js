e43adbcb0e2e931629cbb2410b6a16c82559d8f6
Matt Gray <matt.gray@retrofuzz.com>
2016-07-01 15:16:17 +0100

    Messed about with gulpfile organisation a bit

--- containers/build/gulpfile.js
+++ containers/build/gulpfile.js
@@ -5,16 +5,7 @@ var browserSync = require('browser-sync').create();
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 
-var onError = function (error) {
-
-	var message = '<div style="text-align:left;"><div style="font-weight:bold;">TASK FAILED [' + error.plugin + ']</div>' +
-								'<div>' + error.relativePath + ' : ' + error.line + '</div>' +
-								'<div>' + error.messageOriginal + '</div></div>';
-
-	console.log(error.toString());
-	browserSync.notify(message, 10000);
-	this.emit('end');
-};
+/********** Jobs ***********/
 
 gulp.task('scss', function() {
 	return gulp.src('src/scss/styles.scss')
@@ -29,12 +20,36 @@ gulp.task('scss', function() {
 		.pipe(browserSync.stream({match: "**/*.css"}));
 });
 
+
+
+/********** Entry Points ***********/
+
+gulp.task('build', ['scss'], function() {
+
+});
+
 gulp.task('watch', ['browser-sync'], function() {
   gulp.watch('src/scss/**/*.scss', ['scss']);
 });
+
+
+/********** Helpers ***********/
 
 gulp.task('browser-sync', function() {
     browserSync.init({
         proxy: "nginx"
     });
 });
+
+var onError = function (error) {
+
+	var message = '<div style="text-align:left;">' +
+									'<div style="font-weight:bold;color:red;margin-bottom: 5px;">TASK FAILED [' + error.plugin + ']</div>' +
+									'<div>' + error.relativePath + ' : ' + error.line + '</div>' +
+									'<div>' + error.messageOriginal + '</div>' +
+								'</div>';
+
+	console.log(error.toString());
+	browserSync.notify(message, 10000);
+	this.emit('end');
+};
